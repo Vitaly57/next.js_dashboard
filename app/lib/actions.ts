6069f89ef747eff5a85f712bfc69a701f11927a8
Vitaly57 <zubkov.vitaly2015@yandex.ru>
2024-10-04 09:30:27 +0300

    twelve delite button end

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,8 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 
+
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -33,3 +35,36 @@ const FormSchema = z.object({
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
   }
+
+// Use Zod to update the expected types
+
+  const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+// ---------- deleting button
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
