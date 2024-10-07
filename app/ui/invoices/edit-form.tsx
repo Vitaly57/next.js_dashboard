a886c911730b666d3728fa883f60423232274243
Vitaly57 <zubkov.vitaly2015@yandex.ru>
2024-10-07 16:52:59 +0300

    fourteen end

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -20,10 +21,14 @@ export default function EditInvoiceForm({
 }) {
 
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+ 
+  // return <form action={formAction}></form>;
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
+     {/* <form action={updateInvoiceWithId}> */}
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -87,6 +92,7 @@ export default function EditInvoiceForm({
                   value="pending"
                   defaultChecked={invoice.status === 'pending'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  
                 />
                 <label
                   htmlFor="pending"
@@ -103,6 +109,7 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === 'paid'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  
                 />
                 <label
                   htmlFor="paid"
